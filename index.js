6eb8204e14e08aa89b05fc20f6cda505336e0be3
cavis <ryancavis@gmail.com>
2019-04-23 14:36:30 -0600

    Log pixel trackers; handle edge cases

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const log = require('lambda-log')
+const util = require('./lib/util')
+const tracker = require('./lib/tracker')
 const {base64Pixel, pixelSize} = require('./lib/constants')
+
+const TARGET_PATH = '/i.gif'
+const text = (statusCode, body) => {
+  return {statusCode, body, headers: {'content-type': 'text/plain'}}
+}
 
 /**
  * Serve a 1x1 pixel and log metrics
  */
 exports.handler = async (event) => {
-  if (event.path === '/i.gif') {
-    return {
-      statusCode: 200,
-      body: base64Pixel,
-      isBase64Encoded: true,
-      headers: {'content-type': 'image/gif', 'content-length': pixelSize},
+  try {
+    if (event.path === TARGET_PATH && ['GET', 'HEAD'].indexOf(event.httpMethod) > -1) {
+      const headers = util.keysToLowerCase(event.headers)
+      const identity = (event.requestContext || {}).identity || {}
+      const query = event.queryStringParameters || {}
+
+      await tracker.log(
+        query['k'],
+        query['c'],
+        headers['user-agent'],
+        headers['x-forwarded-for'] || identity['sourceIp'],
+        headers['referer'],
+      )
+
+      return {
+        statusCode: 200,
+        body: base64Pixel,
+        isBase64Encoded: true,
+        headers: {'content-type': 'image/gif', 'content-length': pixelSize},
+      }
+    } else if (event.path === TARGET_PATH) {
+      return text(405, 'Method not allowed')
+    } else {
+      return text(404, 'Pixel not found')
     }
-  } else {
-    return {
-      statusCode: 404,
-      body: 'Pixel not found',
-      headers: {'content-type': 'text/plain'},
-    }
+  } catch (err) {
+    log.error(err)
+    return text(500, `Well that didn't work`)
   }
 }
--- index.test.js
+++ index.test.js
@@ -1,21 +1,103 @@
+jest.mock('lambda-log')
+jest.mock('./lib/tracker')
+
+const log = require('lambda-log')
+const tracker = require('./lib/tracker')
 const {handler} = require('./index')
 
 describe('index', () => {
 
-  it('returns 404s for unknown paths', async () => {
-    expect(await handler({})).toMatchObject({statusCode: 404})
-    expect(await handler({path: '/any/path'})).toMatchObject({statusCode: 404})
-    expect(await handler({path: '/any/i.gif'})).toMatchObject({statusCode: 404})
-    expect(await handler({path: '/i.png'})).toMatchObject({statusCode: 404})
-  })
+  const event = {
+    httpMethod: 'GET',
+    path: '/i.gif',
+    headers: {
+      'user-agent': 'my-agent',
+      'x-forwarded-for': '98.76.54.321',
+      'referer': 'https://www.prx.org/',
+    },
+    queryStringParameters: {
+      k: 'my-key-here',
+      c: 'http://the.canonical/url',
+    },
+    requestContext: {
+      identity: {
+        sourceIp: '123.456.78.9'
+      },
+    },
+  }
 
   it('returns a pixel', async () => {
-    const resp = await handler({path: '/i.gif'})
+    const resp = await handler(event)
     expect(resp.statusCode).toEqual(200)
     expect(resp.headers['content-type']).toEqual('image/gif')
     expect(resp.headers['content-length']).toEqual(35)
     expect(resp.isBase64Encoded).toEqual(true)
     expect(new Buffer(resp.body, 'base64').byteLength).toEqual(35)
+  })
+
+  it('tracks impressions', async () => {
+    expect(await handler(event)).toMatchObject({statusCode: 200})
+    expect(tracker.__log.length).toEqual(1)
+    expect(tracker.__log[0]).toEqual([
+      'my-key-here',
+      'http://the.canonical/url',
+      'my-agent',
+      '98.76.54.321',
+      'https://www.prx.org/',
+    ])
+  })
+
+  it('lowercases header values', async () => {
+    const headers = {'User-Agent': 'agent2', 'x-FORwarDED-for': 'ip2', 'Referer': 'ref2'}
+    expect(await handler({...event, headers})).toMatchObject({statusCode: 200})
+    expect(tracker.__log.length).toEqual(1)
+    expect(tracker.__log[0]).toEqual([
+      'my-key-here',
+      'http://the.canonical/url',
+      'agent2',
+      'ip2',
+      'ref2',
+    ])
+  })
+
+  it('responds to HEAD requests', async () => {
+    expect(await handler({...event, httpMethod: 'HEAD'})).toMatchObject({statusCode: 200})
+  })
+
+  it('returns 405 for bad methods', async () => {
+    expect(await handler({...event, httpMethod: 'PUT'})).toMatchObject({statusCode: 405})
+    expect(await handler({...event, httpMethod: 'PATCH'})).toMatchObject({statusCode: 405})
+    expect(await handler({...event, httpMethod: 'POST'})).toMatchObject({statusCode: 405})
+    expect(await handler({...event, httpMethod: 'DELETE'})).toMatchObject({statusCode: 405})
+    expect(await handler({...event, httpMethod: 'OPTIONS'})).toMatchObject({statusCode: 405})
+  })
+
+  it('returns 404s for unknown paths', async () => {
+    expect(await handler({})).toMatchObject({statusCode: 404})
+    expect(await handler({...event, path: '/any/path'})).toMatchObject({statusCode: 404})
+    expect(await handler({...event, path: '/any/i.gif'})).toMatchObject({statusCode: 404})
+    expect(await handler({...event, path: '/i.png'})).toMatchObject({statusCode: 404})
+  })
+
+  it('falls back to the source ip', async () => {
+    const headers = {...event.headers, 'x-forwarded-for': undefined}
+    expect(await handler({...event, headers})).toMatchObject({statusCode: 200})
+    expect(tracker.__log.length).toEqual(1)
+    expect(tracker.__log[0]).toEqual([
+      'my-key-here',
+      'http://the.canonical/url',
+      'my-agent',
+      '123.456.78.9',
+      'https://www.prx.org/',
+    ])
+  })
+
+  it('catches and logs errors', async () => {
+    const err = new Error('something went horribly wrong')
+    jest.spyOn(tracker, 'log').mockImplementation(async () => { throw err })
+    expect(await handler(event)).toMatchObject({statusCode: 500})
+    expect(log.__error.length).toEqual(1)
+    expect(log.__error[0].msg).toEqual(err)
   })
 
 })
