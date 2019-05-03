01848beaed65ddd738e352f067dc857c49b72287
cavis <ryancavis@gmail.com>
2019-05-03 11:58:53 -0600

    Attempt to redirect and set a cookie

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const cookie = require('cookie')
 const log = require('lambda-log')
+const uuidv4 = require('uuid/v4')
 const util = require('./lib/util')
 const pages = require('./lib/pages')
 const tracker = require('./lib/tracker')
+const COOKIE_NAME = '_pxid'
 
 /**
  * Serve a 1x1 pixel and log metrics
@@ -11,6 +14,7 @@ exports.handler = async (event) => {
     const query = event.queryStringParameters || {}
     const headers = util.keysToLowerCase(event.headers) || {}
     const identity = (event.requestContext || {}).identity || {}
+    const userId = cookie.parse(headers['cookie'] || '')[COOKIE_NAME]
 
     if (event.httpMethod !== 'GET' && event.httpMethod !== 'HEAD') {
       return util.text(405, 'Method not allowed')
@@ -18,9 +22,15 @@ exports.handler = async (event) => {
       return pages.home()
     } else if (event.path === '/admin') {
       return await pages.admin(query, headers)
-    } else if (event.path === '/i.gif') {
-      await tracker.log(query, headers, identity)
+    } else if ((event.path === '/i.gif' && userId) || event.path === '/r.gif') {
+      await tracker.log(query, headers, identity, userId)
       return util.pixel()
+    } else if (event.path === '/i.gif') {
+      const params = util.encodeQuery(query)
+      const domain = headers['host']
+      const expires = util.future(1)
+      const setCookie = cookie.serialize(COOKIE_NAME, uuidv4(), {domain, expires})
+      return util.redirect(`/r.gif${params}`, setCookie)
     } else {
       return util.text(404, 'Pixel not found')
     }
